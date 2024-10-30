21445f7bcf6f9807a909576cad81312cc010d341
Victor <60045243+wizardassassin@users.noreply.github.com>
2024-10-30 00:28:06 -0700

    Added weekly, monthly, yearly, guild recaps

--- src/query-informer.ts
+++ src/query-informer.ts
@@ -1,4 +1,6 @@
 import { Client } from "discord.js";
+import { DateTime } from "luxon";
+import { createGuildRecap } from "./recap-format.js";
 
 const guildId = process.env.DISCORD_GUILD_ID;
 
@@ -15,4 +17,56 @@ export async function onCron(
     await message.edit(
         "Fetching data..." + (didSucceed ? "Success" : "Failed")
     );
+    if (didSucceed) {
+        const dateObj = DateTime.now().setZone("America/New_York");
+        const dateToday = dateObj.startOf("day");
+        if (isStartOfWeek(dateObj)) {
+            await createGuildRecap(
+                guildId,
+                dateToday.minus({ weeks: 1 }),
+                dateToday,
+                "Weekly Guild Recap",
+                (content) => client.instanceChannel.send(content)
+            );
+        }
+        if (isStartOfMonth(dateObj)) {
+            await createGuildRecap(
+                guildId,
+                dateToday.minus({ months: 1 }),
+                dateToday,
+                "Monthly Guild Recap",
+                (content) => client.instanceChannel.send(content)
+            );
+        }
+        if (isStartOfYear(dateObj)) {
+            await createGuildRecap(
+                guildId,
+                dateToday.minus({ years: 1 }),
+                dateToday,
+                "Yearly Guild Recap",
+                (content) => client.instanceChannel.send(content)
+            );
+        }
+    }
+}
+
+function isStartOfYear(dateObj: DateTime<true> | DateTime<false>) {
+    const dateYear = dateObj.startOf("year");
+    const dateToday = dateObj.startOf("day");
+    return dateYear.toMillis() === dateToday.toMillis();
+}
+
+function isStartOfMonth(dateObj: DateTime<true> | DateTime<false>) {
+    const dateMonth = dateObj.startOf("month");
+    const dateToday = dateObj.startOf("day");
+    return dateMonth.toMillis() === dateToday.toMillis();
+}
+
+function isStartOfWeek(dateObj: DateTime<true> | DateTime<false>) {
+    const dateWeek = dateObj
+        .plus({ days: 1 })
+        .startOf("week")
+        .minus({ days: 1 });
+    const dateToday = dateObj.startOf("day");
+    return dateWeek.toMillis() === dateToday.toMillis();
 }
