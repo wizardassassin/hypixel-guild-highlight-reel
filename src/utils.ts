84bd95d1096ac3ea7e8ccb74879cd3397ec5e12f
Victor <60045243+wizardassassin@users.noreply.github.com>
2025-04-22 00:17:30 -0700

    Updated retryer code

--- src/utils.ts
+++ src/utils.ts
@@ -1,17 +1,23 @@
 export const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
+export const MAX_DEPTH = 7;
+
 export async function simpleRetryer<T>(
     func: () => Promise<T>,
-    waitTime = 5000,
-    depth = 0
+    depth = 0,
+    delay = [1000, 5000, 30000, 60000, 300000]
 ) {
     try {
         return await func();
     } catch (error) {
-        if (depth >= 5) throw error;
+        if (depth >= MAX_DEPTH) throw error;
         console.error(error);
-        console.error(`Retrying in ${waitTime / 1000} seconds`);
-        await sleep(waitTime);
-        return await simpleRetryer(func, waitTime, depth + 1);
+        console.error(`Retrying in ${delay[0] / 1000} seconds`);
+        await sleep(delay[0]);
+        return await simpleRetryer(
+            func,
+            depth + 1,
+            delay.length > 1 ? delay.slice(1) : delay
+        );
     }
 }
